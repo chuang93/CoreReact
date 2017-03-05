55cd09d00958f080427e2026ebbab309776d9252
chuang93 <chuang93@gmail.com>
2017-03-04 23:52:26 -0800

    reference after gulpfile

--- src/CoreReact/wwwroot/_references.js
+++ src/CoreReact/wwwroot/_references.js
@@ -1,4 +1,5 @@
 ﻿/// <autosync enabled="true" />
+/// <reference path="../gulpfile.js" />
 /// <reference path="js/playerprofiles.js" />
 /// <reference path="js/playerprofilewebapi.js" />
 /// <reference path="js/site.js" />
